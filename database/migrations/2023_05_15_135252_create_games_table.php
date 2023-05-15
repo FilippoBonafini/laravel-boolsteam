7fa3f54f23ff83ab162d9b88b6c910efdb6a7e9b
danielebd <danielebalebalestro@gmail.com>
2023-05-15 17:28:38 +0200

    seed

--- database/migrations/2023_05_15_135252_create_games_table.php
+++ database/migrations/2023_05_15_135252_create_games_table.php
@@ -17,13 +17,13 @@ return new class extends Migration
             $table->id();
 
             $table->string('title',100);
-            $table->json('genres');
+            $table->string('genres');
             $table->integer('release_year');
             $table->text('description');
             $table->string('developer',100);
-            $table->json('platforms');
+            $table->string('platforms');
             $table->boolean('crossplay')->default(false);
-            $table->json('languages',100);
+            $table->string('languages',100);
             $table->boolean('online')->default(false);
             $table->float('price',6, 2);
             $table->text('cover');
--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Game;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,24 @@ class GameSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $games = config('games');
+
+        foreach($games as $game){
+            $newGame = new Game();
+
+            $newGame->title = $game['title'];
+            $newGame->genres = implode(',',$game['genres']);
+            $newGame->release_year = $game['release_year'];
+            $newGame->description = $game['description'];
+            $newGame->developer = $game['developer'];
+            $newGame->platforms = implode(',',$game['platforms']);
+            $newGame->crossplay = $game['crossplay'];
+            $newGame->languages = implode(',',$game['languages']);
+            $newGame->online = $game['online'];
+            $newGame->price = $game['price'];
+            $newGame->cover = $game['cover'];
+
+            $newGame->save();
+        }
     }
 }
