ac3a8baa3401f0a41e28877d0261de4c9cf72f43
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-05-15 18:03:42 +0200

    corretta grafica

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -8,7 +8,6 @@
                 <th scope="col">Title</th>
                 <th scope="col">Price</th>
                 <th scope="col">Casa</th>
-                <th scope="col">Edit</th>
                 <th scope="col"></th>
             </tr>
         </thead>
@@ -17,10 +16,8 @@
                 <td class=" align-middle">titolo</td>
                 <td class=" align-middle">prezzo</td>
                 <td class="align-middle">casa</td>
-                <td class="align-middle">
+                <td class="align-middle d-flex justify-content-end align-items-center gap-3">
                     @include('partials.editZone')
-                </td>
-                <td class="align-middle">
                     <a href="/">
                         @include('partials.svg.show')
                     </a>
