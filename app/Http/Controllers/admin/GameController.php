a2798a976c740e9a70a3816d22af9f33ea62fb6c
Your Name <you@example.com>
2023-05-15 17:51:29 +0200

    update e destroy

--- app/Http/Controllers/admin/GameController.php
+++ app/Http/Controllers/admin/GameController.php
@@ -72,7 +72,7 @@ class GameController extends Controller
     public function update(UpdateGameRequest $request, Game $game)
     {
         $data = $request->validated();
-        $game->update($game);
+        $game->update($data);
         return redirect()->route('games.index', $Game->id);
     }
 
