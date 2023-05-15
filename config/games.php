1e8b1c8d8a402a395af9a8074c1d2726ca424ac8
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-05-15 16:04:59 +0200

    inizializzato progetto

---
+++ config/games.php
@@ -0,0 +1,81 @@
+<?php
+return [
+    [
+        'title' => 'The Legend of Zelda: Breath of the Wild',
+        'genres' => array('Action', 'Adventure'),
+        'release_year' => 2017,
+        'description' => 'The Legend of Zelda: Breath of the Wild is an exciting action-adventure game set in a vast open world. Explore breathtaking landscapes, solve puzzles, battle enemies, and uncover the secrets of the kingdom of Hyrule. With its immersive gameplay and stunning visuals, this game offers an unforgettable journey for players.',
+        'developer' => 'Nintendo',
+        'platforms' => array('Nintendo Switch', 'Wii U'),
+        'crossplay' => false,
+        'languages' => array('English', 'Italian', 'French', 'Spanish', 'Other'),
+        'online' => false,
+        'price' => 60,
+        'cover' => 'https://static.posters.cz/image/1300/stampa-su-tela-the-legend-of-zelda-breath-of-the-wild-view-i111060.jpg'
+    ],
+    [
+        'title' => 'Red Dead Redemption 2',
+        'genres' => array('Action', 'Adventure', 'Open-world'),
+        'release_year' => 2018,
+        'description' => 'Red Dead Redemption 2 takes players on an epic journey through the Wild West. Play as Arthur Morgan, an outlaw on the run, and experience a rich story filled with memorable characters and intense action. From gunfights to horseback riding, hunting, and exploring the vast open world, this game offers a truly immersive and cinematic experience.',
+        'developer' => 'Rockstar Games',
+        'platforms' => array('PlayStation 4', 'Xbox One', 'PC'),
+        'crossplay' => false,
+        'languages' => array('English', 'Italian', 'French', 'Spanish', 'Other'),
+        'online' => true,
+        'price' => 50,
+        'cover' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gtabase.com%2Fred-dead-redemption-2%2Fcover-art%2F&psig=AOvVaw3mShKGO8QLmRZ2Ebc_Us9F&ust=1684242262258000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMDb-r-x9_4CFQAAAAAdAAAAABAE'
+    ],
+    [
+        'title' => 'The Witcher 3: Wild Hunt',
+        'genres' => array('Action', 'Role-playing'),
+        'release_year' => 2015,
+        'description' => 'The Witcher 3: Wild Hunt is a critically acclaimed role-playing game set in a dark fantasy world. Step into the shoes of Geralt of Rivia, a monster hunter, and embark on a quest to find your adopted daughter. With its immersive storytelling, complex characters, and dynamic combat system, this game offers a captivating and immersive experience for players.',
+        'developer' => 'CD Projekt',
+        'platforms' => array('PlayStation 4', 'Xbox One', 'PC', 'Nintendo Switch'),
+        'crossplay' => false,
+        'languages' => array('English', 'Italian', 'French', 'Spanish', 'Other'),
+        'online' => false,
+        'price' => 20,
+        'cover' => 'https://cdn1.epicgames.com/offer/14ee004dadc142faaaece5a6270fb628/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S2_1200x1600-53a8fb2c0201cd8aea410f2a049aba3f'
+    ],
+    [
+        'title' => 'Grand Theft Auto V',
+        'genres' => array('Action', 'Open-world'),
+        'release_year' => 2013,
+        'description' => 'Grand Theft Auto V is an open-world action game set in the sprawling city of Los Santos. Play as three different characters, each with their own unique abilities and stories, and engage in a variety of missions, heists, and activities. With its immersive world, engaging gameplay, and a massive online multiplayer mode, GTA V offers endless hours of entertainment.',
+        'developer' => 'Rockstar Games',
+        'platforms' => array('PlayStation 4', 'PlayStation 3', 'Xbox One', 'Xbox 360', 'PC'),
+        'crossplay' => false,
+        'languages' => array('English', 'Italian', 'French', 'Spanish', 'Other'),
+        'online' => true,
+        'price' => 20,
+        'cover' => 'https://i.pinimg.com/originals/7b/34/81/7b34815378a30fb718bcdb97b0a1d178.jpg'
+    ],
+    [
+        'title' => 'Minecraft',
+        'genres' => array('Sandbox', 'Survival'),
+        'release_year' => 2011,
+        'description' => 'Minecraft is a sandbox survival game that allows players to build and explore virtual worlds made up of blocks. With its creative mode, players can unleash their imagination and construct anything they can dream of. In survival mode, players must gather resources, craft tools, and fend off enemies. Minecraft offers endless possibilities and has become a global phenomenon.',
+        'developer' => 'Mojang Studios',
+        'platforms' => array('PC', 'PlayStation 4', 'Xbox One', 'Nintendo Switch', 'Mobile'),
+        'crossplay' => true,
+        'languages' => array('English', 'Italian', 'French', 'Spanish', 'Other'),
+        'online' => true,
+        'price' => 25,
+        'cover' => 'https://www.codiciprodotto.it/wp-content/uploads/2021/01/Minecraft-Starter-Collection-XboxOne-COVER.jpg'
+    ],
+    [
+        'title' => 'Super Mario Odyssey',
+        'genres' => array('Platformer', 'Action-adventure'),
+        'release_year' => 2017,
+        'description' => "Super Mario Odyssey is a charming platformer and action-adventure game featuring everyone's favorite plumber, Mario. Join Mario and his new companion, Cappy, as they travel across various kingdoms in their quest to rescue Princess Peach from Bowser's clutches. With its innovative gameplay mechanics, vibrant worlds, and nostalgic nods to the Mario franchise, this game is a must-play for fans.",
+        'developer' => 'Nintendo',
+        'platforms' => array('Nintendo Switch'),
+        'crossplay' => false,
+        'languages' => array('English', 'Italian', 'French', 'Spanish', 'Other'),
+        'online' => false,
+        'price' => 60,
+        'cover' => 'https://www.gamereactor.it/media/84/supermarioodyssey_coveret_2158443b.jpg'
+    ]
+];
