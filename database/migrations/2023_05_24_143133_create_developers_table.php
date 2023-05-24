394b925dbf7b9be2d0e8b466198a54beffb46709
alessandroiero <alessandro.iero@outlook.it>
2023-05-24 18:20:10 +0200

    Problema Migration Seeder

--- database/migrations/2023_05_24_143133_create_developers_table.php
+++ database/migrations/2023_05_24_143133_create_developers_table.php
@@ -3,11 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
-
-
-return new class extends Migration
+new class extends Migration
 {
     /**
      * Run the migrations.
@@ -18,11 +17,9 @@ return new class extends Migration
     {
         Schema::create('developers', function (Blueprint $table) {
             $table->id();
-
             $table->string('name', 70)->unique();
             $table->string('location', 100);
-            $table->string('slug');
-
+            $table->string('slug');            
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            DeveloperSeeder::class,
-            GameSeeder::class
+            GameSeeder::class,
+            DeveloperSeeder::class
         ]);
     }
 }
