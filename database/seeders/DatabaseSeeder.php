3c5db9e66955fb2a9b63bd5674653190541204db
danielebd <danielebalebalestro@gmail.com>
2023-05-24 17:09:16 +0200

    seeder genre

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this-> call (GameSeeder::class);
+        $this-> call (GenreSeeder::class);
     }
 }
--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Game;
+use App\Models\Genre;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class GenreSeeder extends Seeder
 {
@@ -14,6 +18,31 @@ class GenreSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //disattiva relazioni
+        Schema::disableForeignKeyConstraints();
+
+        //elimina dati
+        Game::truncate();
+
+        //riattiva relazioni
+        Schema::enableForeignKeyConstraints();
+
+        $genres = [];
+        $data = config('games');
+
+        foreach ($data as $game) {
+            foreach ($game['genres'] as $genre) {
+                if (!in_array($genre, $genres)) {
+                    array_push($genres, $genre);
+                }
+            }
+        }
+        foreach ($genres as $genre) {
+
+            $new_genre = new Genre();
+            $new_genre->name = $genre;
+            $new_genre->slug = Str::slug($new_genre->name);
+            $new_genre->save();
+        }
     }
 }
