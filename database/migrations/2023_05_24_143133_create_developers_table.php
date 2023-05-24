3937378f81ef153be52aa27403bd2ddcc79082dd
LucaPaolella <paolellaluca99@gmail.com>
2023-05-24 17:41:02 +0200

    db seed

--- database/migrations/2023_05_24_143133_create_developers_table.php
+++ database/migrations/2023_05_24_143133_create_developers_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
 
 return new class extends Migration
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this-> call (GameSeeder::class);
+        $this->call([
+            DeveloperSeeder::class,
+            GameSeeder::class
+        ]);
     }
 }
