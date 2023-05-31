25d11184eb9f83a6f8894385ebaf2adb8c3383c1
DavideAtzo <atzo9477@gmail.com>
2023-05-31 17:15:05 +0200

    fine crud platforms

--- resources/views/admin/platforms/edit.blade.php
+++ resources/views/admin/platforms/edit.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.auth')
 
 @section('content')
-    <form action="{{ route('admin.platforms.store') }}" method="POST" class="container" id="save-form"
+    {{-- errori --}}
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    {{-- errori --}}
+
+    <form action="{{ route('admin.platforms.update', $platform->id) }}" method="POST" class="container" id="save-form"
         enctype="multipart/form-data">
         @csrf
+        {{-- metodo put patch --}}
+        @method('PUT')
         <div class="pt-5 row justify-content-center">
             <div class="col">
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
                 <div class="card">
                     <div class="card-header d-flex gap-3 align-items-center justify-content-between">
                         <input type="text" class="form-control" id="name" name="name"
